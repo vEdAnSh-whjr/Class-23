4433a8c4064c70f5feb15bd8e60e8b159ca70884
vEdAnSh-whjr <76542652+vEdAnSh-whjr@users.noreply.github.com>
2021-01-20 14:12:00 +1100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Class-23
-blueprints
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ box.js
@@ -0,0 +1,16 @@
+class Box {
+constructor(x, y, width, height){
+  var options ={
+     restitution: 2.0
+  }
+  this.body = Bodies.rectangle(x, y, width, height, options)
+  this.width = width
+  this.height = height
+  World.add (world, this.body)
+}
+display(){
+   rectMode(CENTER) 
+    var pos = this.body.position
+    rect(pos.x, pos.y,this.width, this.height)
+}
+}
